c4c1ce2356fcc481e14dc6771ac4f6eb83f2d0e5
michalper <michal.per@outlook.com>
2019-07-22 22:26:47 +0200

    sonarcloud smell code fix

--- lib/Statscore/Service/Exception/AuthorizationException.php
+++ lib/Statscore/Service/Exception/AuthorizationException.php
@@ -6,18 +6,6 @@ use Exception;
 
 class AuthorizationException extends Exception
 {
-
     const ERROR_AUTHORIZATION_CLIENT_ID = 'Client ID missing.';
     const ERROR_AUTHORIZATION_SECRET_KEY = 'Secret key missing.';
-
-    /**
-     * AuthorizationException constructor.
-     * @param $message
-     * @param int $code
-     * @param Exception|null $previous
-     */
-    public function __construct(string $message, int $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
